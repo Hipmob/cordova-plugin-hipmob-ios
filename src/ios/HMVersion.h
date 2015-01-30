970aa0f21307e04982d7438eff0f85a1bf87e89a
Femi Omojola <femi@hipmob.com>
2015-01-30 13:25:22 -0500

    Added the missing quartzcore framework

--- src/ios/HMVersion.h
+++ src/ios/HMVersion.h
@@ -9,7 +9,7 @@
 #ifndef hipmob_HMVersion_h
 #define hipmob_HMVersion_h
 
-#define HIPMOB_LIBRARY_VERSION              3.9.1
-#define HIPMOB_LIBRARY_VERSION_STRING       @"3.9.1"
+#define HIPMOB_LIBRARY_VERSION              3.9.2
+#define HIPMOB_LIBRARY_VERSION_STRING       @"3.9.2"
 
 #endif
